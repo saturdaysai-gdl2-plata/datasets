1a86a07da527be9b57c838bcd0fc7653550275ae
Iván Galaviz <ivan.galaviz@wizeline.com>
2020-06-20 12:25:48 -0500

    Filter women trips and merge lat, lng to neighborhoods (#11)
    * Filter women trips and merge lat, lng to neighborhoods
    
    * Separate Dataframes in file
    
    * Remove duplicated line
    
    Co-authored-by: erflogo <erflogo@gmail.com>

--- eda_joined.py
+++ eda_joined.py
@@ -29,23 +29,29 @@ df['diff_seconds']= df['diff_seconds']/np.timedelta64(1,'s')
 #Borramos todo lo que este menos de 15 Segundos en la columna diff_seconds
 df.drop(df[df.diff_seconds < 15].index, inplace = True)
 
+# Filtramos los registros para sólo mujeres
+df = df[df["Genero"].isin(["F"])]
+
 estaciones_df = pd.read_csv('https://saturdays-ai-gdl2-plata-mibici.s3-us-west-2.amazonaws.com/estaciones.csv')
 df = df.merge(estaciones_df, left_on='Origen_Id', right_on='id', how= 'left')
 df = df.merge(estaciones_df, left_on='Destino_Id', right_on='id', how= 'left')
 df.drop(['id_x', 'id_y'], inplace = True, axis=1)
 df.rename(columns={"latitud_x": "latitud_origen", "longitud_x": "longitud_origen"}, inplace = True)
 df.rename(columns={"latitud_y": "latitud_destino", "longitud_y": "longitud_destino"}, inplace = True)
+df['mes'] = df['Inicio_del_viaje'].dt.month
+
+# ======== Criminal Incidence ========
 
 df_criminal = pd.read_csv('https://trello-attachments.s3.amazonaws.com/5e7ab7849f172231e1d8b386/5e7d5e0fec43d718240c71c7/cbf8668259455adfbabd3f686e410b41/incidencia_delictiva_jalisco18-19_filter.csv')
 
 municipalities = ["GUADALAJARA", "ZAPOPAN", "SAN PEDRO TLAQUEPAQUE"]
 null_values = ["N.D.", "N..D."]
+crimes = ["LESIONES DOLOSAS", "ROBO DE MOTOCICLETA", "ROBO A CUENTAHABIENTES", "HOMICIDIO DOLOSO", "ROBO A NEGOCIO", "FEMINICIDIO"]
 
 df_criminal = df_criminal[df_criminal["Municipio"].isin(municipalities)]
 df_criminal = df_criminal[~df_criminal["Colonia"].isin(null_values)]
-
-crimes = ["LESIONES DOLOSAS", "ROBO DE MOTOCICLETA", "ROBO A CUENTAHABIENTES", "HOMICIDIO DOLOSO", "ROBO A NEGOCIO", "FEMINICIDIO"]
-
 df_criminal = df_criminal[df_criminal["Delito"].isin(crimes)]
 
-df['mes'] = df['Inicio_del_viaje'].dt.month
+lat_lng = pd.read_csv("https://saturdays-ai-gdl2-plata-mibici.s3-us-west-2.amazonaws.com/neighborhoods_latlng.csv")
+df_criminal = df_criminal.merge(lat_lng, left_on='Colonia', right_on="colonia", how="left")
+df_criminal.drop(['Mes', 'Clave_Mun', 'colonia', 'query', 'status'], axis = 1, inplace=True)
